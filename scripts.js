277a547596dfb00fe00d5912dfb1b54fbb6589ab
singhsushantctps <87160673+singhsushantctps@users.noreply.github.com>
2021-10-03 11:56:21 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,11 +1,25 @@
 // Run the script once the document is completely loaded.
 $(function () {
-    var role = 0;
-    var roles = ["Front End Developer", "Programming Enthusiast"];
-    setInterval(() => {
-      $(".home-content h2 span").fadeOut(500, function () {
-        $(this).text(roles[role++ % roles.length] + "...");
-        $(this).fadeIn(500);
-      });
-    }, 2500);
-  });
+  var role = 0;
+  var roles = ["Front End Developer", "Programming Enthusiast"];
+  setInterval(() => {
+    $(".home-content h2 span").fadeOut(500, function () {
+      $(this).text(roles[role++ % roles.length] + "...");
+      $(this).fadeIn(500);
+    });
+  }, 2500);
+});
+$(function () {
+	$("nav")
+		.css("display", "none")
+		.removeClass("hidden");
+	$(window).scroll(function () {
+		// find the trigger point.
+		var point = $("#home h1").offset().top - 30;
+		if ($(window).scrollTop() > point) {
+			$("nav").slideDown();
+		} else {
+			$("nav").slideUp();
+		}
+	});
+});
