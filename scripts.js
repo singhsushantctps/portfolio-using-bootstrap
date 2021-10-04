60ee7a1394047fdc51563507bac72e4ab42c1133
singhsushantctps <87160673+singhsushantctps@users.noreply.github.com>
2021-10-04 22:50:31 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ $(function () {
   var role = 0;
   var roles = ["Front End Developer", "Programming Enthusiast"];
   setInterval(() => {
-    $(".home-content h2 span").fadeOut(500, function () {
+    $(".home-content h3 span").fadeOut(500, function () {
       $(this).text(roles[role++ % roles.length] + "...");
       $(this).fadeIn(500);
     });
